2b144f909b968a5cb1d99c4d4ae9854377bdd1a9
DrYruo <oury.jordan@hotmail.com>
2022-10-20 12:13:18 +0200

    debut de la faille xSS

--- page/faille-xss.php
+++ page/faille-xss.php
@@ -1,4 +1,12 @@
 <!DOCTYPE html>
+<?php
+function getXssCommentaire() {
+    if(isset($_GET['xssCommentaire'])) {
+        echo "<p>".$_GET['xssCommentaire']."</p>";
+    }
+}
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,14 +16,80 @@
 <body>
 
 <div class="container-fluid">
-    <div class="row bg-info py-4">
+    <div class="row bg-dark py-4">
         <div class="col-5 d-flex align-items-center">
-            <a type="button" class="btn btn-outline-dark px-4 my-auto" href="../index.php">Go Back</a>
+            <a type="button" class="btn btn-outline-light px-4 my-auto" href="../index.php">Go Back</a>
         </div>
         <div class="col-7">
-            <h1>Faille XSS</h1>
+            <h1 class="text-light">Faille XSS</h1>
         </div>
     </div>
 </div>
+
+<div class="container">
+    <div class="row my-5 justify-content-md-center">
+        <div class="col-6 text-center py-2 border border-3 border-dark">
+            <h1>Le cross-site scripting !</h1>
+            <p class="text-lg-start text-lg">Le cross-site scripting (faille XSS) est un type de faille de sécurité des sites web permettant d'injecter du code JavaScript dans une page.
+                <br>Cela permet ainsi d'executer divers actions sur les navigateurs web visitant la page.
+                <br>Les autres utilisateurs exécutent le script sans s’en rendre compte dès l’ouverture de la page.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="col mx-5">
+    <div class="row my-5">
+        <div class="col border border-2 border-danger py-2 bg-dark">
+            <h1 class="text-danger text-center text-danger">Création d'article avec faille</h1>
+            <form action="faille-xss.php" method="get">
+                <div class="border border-1 border-danger py-2 px-2 my-2">
+                    <div class="border border-1 border-danger py-2 px-2 my-2">
+                        <p class="text-light">Blablabla je suis un article super intéressant ! Rajoute vite un commentaire !</p>
+                    </div>
+                    <label for="xssCommentaire" class="text-light">Votre commentaire :</label>
+                    <input type="text" id="xssCommentaire" name="xssCommentaire" class="form-control mb-2">
+                    <div class="mb-3 border border-1 border-danger py-5 px-2 my-2 bg-light">
+                        <?php
+                            getXssCommentaire();
+                        ?>
+                    </div>
+                </div>
+                <button id="btnXsssCommentaireDanger" type="submit" class="form-control btn btn-outline-danger px-4">Envoyer</button>
+            </form>
+        </div>
+        <div class="col mx-5">
+
+        </div>
+    </div>
+
+    <div class="row my-5">
+        <div class="col border border-2 border-success py-2 bg-dark">
+            <h1 class="text-success text-center">Création d'article sans faille</h1>
+            <form action="faille-xss.php" method="get">
+                <div class="border border-1 border-success py-2 px-2 my-2">
+                    <div class="border border-1 border-success py-2 px-2 my-2">
+                        <p class="text-light">Blablabla je suis un article super intéressant ! Rajoute vite un commentaire !</p>
+                    </div>
+                    <label for="xssCommentaire" class="text-light">Votre commentaire :</label>
+                    <input type="text" id="xssCommentaire" name="xssCommentaire" class="form-control mb-2">
+                    <div class="mb-3">
+                        <div class="mb-3 border border-1 border-success py-5 px-2 my-2 bg-light">
+
+                        </div>
+                    </div>
+                </div>
+                <button id="btnXsssCommentaireSuccess" type="submit" class="form-control btn btn-outline-success px-4">Envoyer</button>
+            </form>
+        </div>
+        <div class="col mx-5">
+<!--            <script type='text/javascript'>alert('test')-->
+            <?php
+                echo htmlspecialchars_decode("<script type='text/javascript'>alert('test')")
+            ?>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
